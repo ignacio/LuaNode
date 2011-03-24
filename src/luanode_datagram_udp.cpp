57bb2653489039943214796caa0565ff42257d3e
Ignacio Burgueño <ignaciob@inconcertcc.com>
2011-03-24 12:59:52 -0300

    Silenced "warning: dereferencing type-punned pointer will break strict-aliasing rules"

--- src/luanode_datagram_udp.cpp
+++ src/luanode_datagram_udp.cpp
@@ -11,52 +11,6 @@
 #include <boost/bind.hpp>
 #include "shared_const_buffer.h"
 
-/*// this is needed for tcp_keepalive definition
-#ifdef _WIN32
-#include <winsock2.h>
-#endif
-
-#ifdef _WIN32
-#if defined(_MSC_VER)
-#include "MSTCPiP.h"
-#elif defined(__MINGW32__)
-// afanado de mstcpip.h
-/* Argument structure for SIO_KEEPALIVE_VALS */
-
-/*struct tcp_keepalive {
-	u_long  onoff;
-	u_long  keepalivetime;
-	u_long  keepaliveinterval;
-};
-
-// New WSAIoctl Options
-
-#define SIO_RCVALL            _WSAIOW(IOC_VENDOR,1)
-#define SIO_RCVALL_MCAST      _WSAIOW(IOC_VENDOR,2)
-#define SIO_RCVALL_IGMPMCAST  _WSAIOW(IOC_VENDOR,3)
-#define SIO_KEEPALIVE_VALS    _WSAIOW(IOC_VENDOR,4)
-#define SIO_ABSORB_RTRALERT   _WSAIOW(IOC_VENDOR,5)
-#define SIO_UCAST_IF          _WSAIOW(IOC_VENDOR,6)
-#define SIO_LIMIT_BROADCASTS  _WSAIOW(IOC_VENDOR,7)
-#define SIO_INDEX_BIND        _WSAIOW(IOC_VENDOR,8)
-#define SIO_INDEX_MCASTIF     _WSAIOW(IOC_VENDOR,9)
-#define SIO_INDEX_ADD_MCAST   _WSAIOW(IOC_VENDOR,10)
-#define SIO_INDEX_DEL_MCAST   _WSAIOW(IOC_VENDOR,11)
-
-// Values for use with SIO_RCVALL* options
-#define RCVALL_OFF             0
-#define RCVALL_ON              1
-#define RCVALL_SOCKETLEVELONLY 2
-
-#else
-#error Unknown compiler.
-#endif
-#elif defined(__linux__)
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#endif*/
-
-
 
 using namespace LuaNode::Datagram;
 
