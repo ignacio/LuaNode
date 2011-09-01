50a2de1f17f536029d9c35fd2b01e899e0db6a30
Ignacio Burgueño <ignaciob@inconcertcc.com>
2011-09-01 11:52:02 -0300

    Renamed macro

--- src/luanode_constants.cpp
+++ src/luanode_constants.cpp
@@ -3,14 +3,13 @@
 #include "luanode_constants.h"
 #include <errno.h>
 
-#define BOOST_ERRNO_CASE(e, v)  lua_pushinteger(L, boost::asio::error::e); lua_setfield(L, t, #v);
+#define SET_BOOST_ERR_FIELD(e, v)  lua_pushinteger(L, boost::asio::error::e); lua_setfield(L, t, #v);
 
 #if defined (_WIN32)
-# define ERRNO_CASE(e) lua_pushinteger(L, WSA ## e); lua_setfield(L, t, #e);
+# define SET_ERR_FIELD(e) lua_pushinteger(L, WSA ## e); lua_setfield(L, t, #e);
 #else
-# define ERRNO_CASE(e) lua_pushinteger(L, e); lua_setfield(L, t, #e);
+# define SET_ERR_FIELD(e) lua_pushinteger(L, e); lua_setfield(L, t, #e);
 #endif
-
 
 void LuaNode::DefineConstants(lua_State* L)
 {
@@ -19,82 +18,84 @@ void LuaNode::DefineConstants(lua_State* L)
 
 	lua_pushinteger(L, boost::asio::error::eof); lua_setfield(L, t, "EOF");
 
-	BOOST_ERRNO_CASE(access_denied, EACCES);
-	BOOST_ERRNO_CASE(address_family_not_supported, EAFNOSUPPORT);
-	BOOST_ERRNO_CASE(address_in_use, EADDRINUSE);
-	BOOST_ERRNO_CASE(already_connected, EISCONN);
-	BOOST_ERRNO_CASE(already_started, EALREADY);
-	BOOST_ERRNO_CASE(broken_pipe, EPIPE);
-	BOOST_ERRNO_CASE(connection_aborted, ECONNABORTED);
-	BOOST_ERRNO_CASE(connection_refused, ECONNREFUSED);
-	BOOST_ERRNO_CASE(connection_reset, ECONNRESET);
-	BOOST_ERRNO_CASE(bad_descriptor, EBADF);
-	BOOST_ERRNO_CASE(fault, EFAULT);
-	BOOST_ERRNO_CASE(host_unreachable, EHOSTUNREACH);
-	BOOST_ERRNO_CASE(in_progress, EINPROGRESS);
-	BOOST_ERRNO_CASE(interrupted, EINTR);
-	BOOST_ERRNO_CASE(invalid_argument, EINVAL);
-	BOOST_ERRNO_CASE(message_size, EMSGSIZE);
-	BOOST_ERRNO_CASE(name_too_long, ENAMETOOLONG);
-	BOOST_ERRNO_CASE(network_down, ENETDOWN);
-	BOOST_ERRNO_CASE(network_reset, ENETRESET);
-	BOOST_ERRNO_CASE(network_unreachable, ENETUNREACH);
-	BOOST_ERRNO_CASE(no_descriptors, EMFILE);
-	BOOST_ERRNO_CASE(no_buffer_space, ENOBUFS);
-	BOOST_ERRNO_CASE(no_memory, ENOMEM);
-	BOOST_ERRNO_CASE(no_permission, EPERM);
-	BOOST_ERRNO_CASE(no_protocol_option, ENOPROTOOPT);
-	BOOST_ERRNO_CASE(not_connected, ENOTCONN);
-	BOOST_ERRNO_CASE(not_socket, ENOTSOCK);
-	BOOST_ERRNO_CASE(operation_aborted, ECANCELED);
-	BOOST_ERRNO_CASE(operation_not_supported, EOPNOTSUPP);
-	BOOST_ERRNO_CASE(shut_down, ESHUTDOWN);
-	BOOST_ERRNO_CASE(timed_out, ETIMEDOUT);
-	BOOST_ERRNO_CASE(try_again, EAGAIN);
-	BOOST_ERRNO_CASE(would_block, EWOULDBLOCK);
+	SET_BOOST_ERR_FIELD(access_denied, EACCES);
+	SET_BOOST_ERR_FIELD(address_family_not_supported, EAFNOSUPPORT);
+	SET_BOOST_ERR_FIELD(address_in_use, EADDRINUSE);
+	SET_BOOST_ERR_FIELD(already_connected, EISCONN);
+	SET_BOOST_ERR_FIELD(already_started, EALREADY);
+	SET_BOOST_ERR_FIELD(broken_pipe, EPIPE);
+	SET_BOOST_ERR_FIELD(connection_aborted, ECONNABORTED);
+	SET_BOOST_ERR_FIELD(connection_refused, ECONNREFUSED);
+	SET_BOOST_ERR_FIELD(connection_reset, ECONNRESET);
+	SET_BOOST_ERR_FIELD(bad_descriptor, EBADF);
+	SET_BOOST_ERR_FIELD(fault, EFAULT);
+	SET_BOOST_ERR_FIELD(host_unreachable, EHOSTUNREACH);
+	SET_BOOST_ERR_FIELD(in_progress, EINPROGRESS);
+	SET_BOOST_ERR_FIELD(interrupted, EINTR);
+	SET_BOOST_ERR_FIELD(invalid_argument, EINVAL);
+	SET_BOOST_ERR_FIELD(message_size, EMSGSIZE);
+	SET_BOOST_ERR_FIELD(name_too_long, ENAMETOOLONG);
+	SET_BOOST_ERR_FIELD(network_down, ENETDOWN);
+	SET_BOOST_ERR_FIELD(network_reset, ENETRESET);
+	SET_BOOST_ERR_FIELD(network_unreachable, ENETUNREACH);
+	SET_BOOST_ERR_FIELD(no_descriptors, EMFILE);
+	SET_BOOST_ERR_FIELD(no_buffer_space, ENOBUFS);
+	SET_BOOST_ERR_FIELD(no_memory, ENOMEM);
+	SET_BOOST_ERR_FIELD(no_permission, EPERM);
+	SET_BOOST_ERR_FIELD(no_protocol_option, ENOPROTOOPT);
+	SET_BOOST_ERR_FIELD(not_connected, ENOTCONN);
+	SET_BOOST_ERR_FIELD(not_socket, ENOTSOCK);
+	SET_BOOST_ERR_FIELD(operation_aborted, ECANCELED);
+	SET_BOOST_ERR_FIELD(operation_not_supported, EOPNOTSUPP);
+	SET_BOOST_ERR_FIELD(shut_down, ESHUTDOWN);
+	SET_BOOST_ERR_FIELD(timed_out, ETIMEDOUT);
+	SET_BOOST_ERR_FIELD(try_again, EAGAIN);
+	SET_BOOST_ERR_FIELD(would_block, EWOULDBLOCK);
 
-	BOOST_ERRNO_CASE(host_not_found, HOST_NOT_FOUND);
-	BOOST_ERRNO_CASE(host_not_found_try_again, TRY_AGAIN);
-	BOOST_ERRNO_CASE(no_data, NO_DATA);
-	BOOST_ERRNO_CASE(no_recovery, NO_RECOVERY);
+	SET_BOOST_ERR_FIELD(host_not_found, HOST_NOT_FOUND);
+	SET_BOOST_ERR_FIELD(host_not_found_try_again, TRY_AGAIN);
+	SET_BOOST_ERR_FIELD(no_data, NO_DATA);
+	SET_BOOST_ERR_FIELD(no_recovery, NO_RECOVERY);
 
 #ifdef EPROCLIM
-	ERRNO_CASE(EPROCLIM);
+	SET_ERR_FIELD(EPROCLIM);
 #endif
 
 #ifdef WSAEPROCLIM
-	ERRNO_CASE(EPROCLIM);
+	SET_ERR_FIELD(EPROCLIM);
 #endif
 
 #ifdef EDESTADDRREQ
-	ERRNO_CASE(EDESTADDRREQ);
+	SET_ERR_FIELD(EDESTADDRREQ);
 #endif
 
 #ifdef EPROTOTYPE
-	ERRNO_CASE(EPROTOTYPE);
+	SET_ERR_FIELD(EPROTOTYPE);
 #endif
 
 #ifdef EPROTONOSUPPORT
-	ERRNO_CASE(EPROTONOSUPPORT);
+	SET_ERR_FIELD(EPROTONOSUPPORT);
 #endif
 
 #if defined(ESOCKTNOSUPPORT) || defined(WSAESOCKTNOSUPPORT)
-	ERRNO_CASE(ESOCKTNOSUPPORT);
+	SET_ERR_FIELD(ESOCKTNOSUPPORT);
 #endif
 
 #if defined(EPFNOSUPPORT) || defined(WSAEPFNOSUPPORT)
-	ERRNO_CASE(EPFNOSUPPORT);
+	SET_ERR_FIELD(EPFNOSUPPORT);
 #endif
 
 #ifdef EADDRNOTAVAIL
-	ERRNO_CASE(EADDRNOTAVAIL);
+	SET_ERR_FIELD(EADDRNOTAVAIL);
 #endif
 
 #if defined(EHOSTDOWN) || defined(WSAEHOSTDOWN)
-	ERRNO_CASE(EHOSTDOWN);
+	SET_ERR_FIELD(EHOSTDOWN);
 #endif
 
 #if defined(EDISCON) || defined(WSAEDISCON)
-	ERRNO_CASE(EDISCON);
+	SET_ERR_FIELD(EDISCON);
 #endif
 }
+
+#undef SET_ERR_FIELD
