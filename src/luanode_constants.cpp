9b0873195c0f1f5640407f9f72c7a5e54e0e5bd5
Ignacio Burgueño <ignaciob@inconcertcc.com>
2011-09-01 11:45:29 -0300

    Renamed macro

--- src/luanode_constants.cpp
+++ src/luanode_constants.cpp
@@ -2,49 +2,51 @@
 #include "luanode.h"
 #include "luanode_constants.h"
 
-#define ERRNO_CASE(e, v)  lua_pushinteger(L, boost::asio::error::e); lua_setfield(L, t, #v);
+#define SET_ERR_FIELD(e, v)  lua_pushinteger(L, boost::asio::error::e); lua_setfield(L, t, #v);
 
 void LuaNode::DefineConstants(lua_State* L)
 {
 	lua_newtable(L);
 	int t = lua_gettop(L);
 
-	ERRNO_CASE(access_denied, EACCES);
-	ERRNO_CASE(address_family_not_supported, EAFNOSUPPORT);
-	ERRNO_CASE(address_in_use, EADDRINUSE);
-	ERRNO_CASE(already_connected, EISCONN);
-	ERRNO_CASE(already_started, EALREADY);
-	ERRNO_CASE(broken_pipe, EPIPE);
-	ERRNO_CASE(connection_aborted, ECONNABORTED);
-	ERRNO_CASE(connection_refused, ECONNREFUSED);
-	ERRNO_CASE(connection_reset, ECONNRESET);
-	ERRNO_CASE(bad_descriptor, EBADF);
-	ERRNO_CASE(fault, EFAULT);
-	ERRNO_CASE(host_unreachable, EHOSTUNREACH);
-	ERRNO_CASE(in_progress, EINPROGRESS);
-	ERRNO_CASE(interrupted, EINTR);
-	ERRNO_CASE(invalid_argument, EINVAL);
-	ERRNO_CASE(message_size, EMSGSIZE);
-	ERRNO_CASE(name_too_long, ENAMETOOLONG);
-	ERRNO_CASE(network_down, ENETDOWN);
-	ERRNO_CASE(network_reset, ENETRESET);
-	ERRNO_CASE(network_unreachable, ENETUNREACH);
-	ERRNO_CASE(no_descriptors, EMFILE);
-	ERRNO_CASE(no_buffer_space, ENOBUFS);
-	ERRNO_CASE(no_memory, ENOMEM);
-	ERRNO_CASE(no_permission, EPERM);
-	ERRNO_CASE(no_protocol_option, ENOPROTOOPT);
-	ERRNO_CASE(not_connected, ENOTCONN);
-	ERRNO_CASE(not_socket, ENOTSOCK);
-	ERRNO_CASE(operation_aborted, ECANCELED);
-	ERRNO_CASE(operation_not_supported, EOPNOTSUPP);
-	ERRNO_CASE(shut_down, ESHUTDOWN);
-	ERRNO_CASE(timed_out, ETIMEDOUT);
-	ERRNO_CASE(try_again, EAGAIN);
-	ERRNO_CASE(would_block, EWOULDBLOCK);
+	SET_ERR_FIELD(access_denied, EACCES);
+	SET_ERR_FIELD(address_family_not_supported, EAFNOSUPPORT);
+	SET_ERR_FIELD(address_in_use, EADDRINUSE);
+	SET_ERR_FIELD(already_connected, EISCONN);
+	SET_ERR_FIELD(already_started, EALREADY);
+	SET_ERR_FIELD(broken_pipe, EPIPE);
+	SET_ERR_FIELD(connection_aborted, ECONNABORTED);
+	SET_ERR_FIELD(connection_refused, ECONNREFUSED);
+	SET_ERR_FIELD(connection_reset, ECONNRESET);
+	SET_ERR_FIELD(bad_descriptor, EBADF);
+	SET_ERR_FIELD(fault, EFAULT);
+	SET_ERR_FIELD(host_unreachable, EHOSTUNREACH);
+	SET_ERR_FIELD(in_progress, EINPROGRESS);
+	SET_ERR_FIELD(interrupted, EINTR);
+	SET_ERR_FIELD(invalid_argument, EINVAL);
+	SET_ERR_FIELD(message_size, EMSGSIZE);
+	SET_ERR_FIELD(name_too_long, ENAMETOOLONG);
+	SET_ERR_FIELD(network_down, ENETDOWN);
+	SET_ERR_FIELD(network_reset, ENETRESET);
+	SET_ERR_FIELD(network_unreachable, ENETUNREACH);
+	SET_ERR_FIELD(no_descriptors, EMFILE);
+	SET_ERR_FIELD(no_buffer_space, ENOBUFS);
+	SET_ERR_FIELD(no_memory, ENOMEM);
+	SET_ERR_FIELD(no_permission, EPERM);
+	SET_ERR_FIELD(no_protocol_option, ENOPROTOOPT);
+	SET_ERR_FIELD(not_connected, ENOTCONN);
+	SET_ERR_FIELD(not_socket, ENOTSOCK);
+	SET_ERR_FIELD(operation_aborted, ECANCELED);
+	SET_ERR_FIELD(operation_not_supported, EOPNOTSUPP);
+	SET_ERR_FIELD(shut_down, ESHUTDOWN);
+	SET_ERR_FIELD(timed_out, ETIMEDOUT);
+	SET_ERR_FIELD(try_again, EAGAIN);
+	SET_ERR_FIELD(would_block, EWOULDBLOCK);
 
-	ERRNO_CASE(host_not_found, HOST_NOT_FOUND);
-	ERRNO_CASE(host_not_found_try_again, TRY_AGAIN);
-	ERRNO_CASE(no_data, NO_DATA);
-	ERRNO_CASE(no_recovery, NO_RECOVERY);
+	SET_ERR_FIELD(host_not_found, HOST_NOT_FOUND);
+	SET_ERR_FIELD(host_not_found_try_again, TRY_AGAIN);
+	SET_ERR_FIELD(no_data, NO_DATA);
+	SET_ERR_FIELD(no_recovery, NO_RECOVERY);
 }
+
+#undef SET_ERR_FIELD
