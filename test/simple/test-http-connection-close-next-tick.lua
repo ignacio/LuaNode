f7311dd484c7d2e190e707b033983a2fc5e17ee5
Ignacio Burgueño <ignaciob@inconcertcc.com>
2011-12-15 18:36:16 -0200

    Removed commented code.

--- test/simple/test-http-connection-close-next-tick.lua
+++ test/simple/test-http-connection-close-next-tick.lua
@@ -6,18 +6,14 @@ local net = require("luanode.net")
 
 function test()
 local body = "hello world\n"
-local headers = {connection = 'close'}
 
 local server = http.createServer(function (self, req, res)
-	--setTimeout(function()
 	process.nextTick(function()
 		res:writeHead(200, {["Content-Length"] = #body})
 		res:write(body)
 		res:finish()
 	end)
 end)
-
-local connectCount = 0
 
 server:listen(common.PORT, function ()
 	local c = net.createConnection(common.PORT)
@@ -37,25 +33,6 @@ server:listen(common.PORT, function ()
 		c:finish()
 		server:close()
 	end)
-	--[[
-	local client = http.createClient(common.PORT)
-
-	client:addListener("connect", function ()
-		common.error("CONNECTED")
-		connectCount = connectCount + 1
-	end)
-
-	local request = client:request("GET", "/", headers)
-	request:finish()
-	request:addListener('response', function (self, response)
-		common.error('response start')
-
-		response:addListener("end", function ()
-			common.error('response end')
-			server:close()
-		end)
-	end)
-	--]]
 end)
 
 process:addListener('exit', function ()
