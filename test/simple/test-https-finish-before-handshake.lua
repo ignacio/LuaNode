c2e11bbf7e117c935f1bfd89be1369537a57d2d6
Ignacio Burgueño <ignaciob@inconcertcc.com>
2012-01-30 14:10:08 -0200

    Re-enabled test-https-finish-before-handshake.lua

--- test/simple/test-https-finish-before-handshake.lua
+++ test/simple/test-https-finish-before-handshake.lua
@@ -7,42 +7,25 @@ local crypto = require ("luanode.crypto")
 
 function test()
 
-local caPem = fs.readFileSync(common.fixturesDir .. "/test_ca.pem", 'ascii')
-local certPem = fs.readFileSync(common.fixturesDir .. "/test_cert.pem", 'ascii')
-local keyPem = fs.readFileSync(common.fixturesDir .. "/test_key.pem", 'ascii')
-
-local context = crypto.createContext{key = keyPem, cert = certPem, ca = caPem}
-
 local google = http.createClient(443, "encrypted.google.com", true)
 local request = google:request("GET", "/", { Host = "encrypted.google.com"})
 
---[[
-google:addListener("connect", function () 
-	console.info("connected")
-	--google:setSecure(context)
-end)
---]]
-
---setTimeout(function()
-	--request:finish("")
---end, 2000)
 
 request:finish("")
 
 google:on("secure", function ()
+	console.log("secure")
 	local verified = google:verifyPeer()
 	local peerDN = google:getPeerCertificate()
-	console.log(verified)
+	
+	assert_true(verified)
 		
 	console.log(peerDN.subject)
 	console.log(peerDN.issuer)
 
 	console.log(peerDN.valid_from)
-	console.log(peerDN.valid_to)	-- should extend this
+	console.log(peerDN.valid_to)
 	console.log(peerDN.fingerprint)
-	--c:write( "GET /hello?hello=world&foo=bar HTTP/1.1\r\n\r\n" );
-	--requests_sent = requests_sent + 1
-	--request:finish()
 end)
 
 
@@ -64,11 +47,6 @@ request:on('response', function (self, response)
 	
 	response_received = true
 	timer:stop()
-	--clearInterval(timer)
-	
-	response:on('data', function (self, chunk)
-		console.log('BODY: %s', chunk)
-	end)
 end)
 
 
