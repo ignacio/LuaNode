9374e9d2de1dc2d4b7726c3261b1930d060fd852
Ignacio Burgueño <ignaciob@inconcertcc.com>
2012-03-17 00:09:32 -0300

    Use debug version when running the tests

--- test/run.sh
+++ test/run.sh
@@ -9,7 +9,7 @@ for i in $( ls -1 --hide=disabled simple ); do
 	#valgrind --error-exitcode=1 --quiet --gen-suppressions=yes --tool=helgrind luanode run.lua simple.$i
 	#valgrind --error-exitcode=1 --db-attach=yes --quiet --suppressions=suppressions.supp --leak-check=full --show-reachable=yes --tool=memcheck luanode run.lua simple.$i
 	#valgrind --error-exitcode=1 --num-callers=30 --leak-check=full --show-reachable=yes --suppressions=suppressions.supp --gen-suppressions=yes luanode run.lua simple.$i
-	luanode run.lua simple.$i
+	luanode_d run.lua simple.$i
 
 	if [ $? -ne 0 ]
 	then
