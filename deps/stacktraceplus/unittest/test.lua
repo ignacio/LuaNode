930b19ea731e7d2098612c2781dda62d46298c9f
Ignacio Burgueño <ignaciob@inconcertcc.com>
2013-09-18 12:46:32 -0300

    Updated StackTracePlus to latest commit.
    commit 73254dc67be6103c2cabdd148bdb2ce3918374db
    Author: Ignacio Burgueño <ignaciob@inconcertcc.com>
    Date:   Wed Sep 18 12:22:53 2013 -0300
    
        Get the stack trace of the current or the given thread (#1)
    
        The stack trace wasn't being generated properly. I wasn't taking the
        current thread int oaccount when building it, so the main thread was
        the one being used.
    
        `stacktrace` now accepts the same parameters as `debug.traceback`
    
        fixes #1

--- deps/stacktraceplus/unittest/test.lua
+++ deps/stacktraceplus/unittest/test.lua
@@ -24,3 +24,24 @@ function testKnownTable()
 	local ok, err = xpcall(f, STP.stacktrace)
 	assert_match( [[ = this is my table]], err, "" )
 end
+
+function testCoroutine()
+	local var_outside_co = {}
+
+	local function helper()
+		local arg_helper = "hi there!"
+		error("an error")
+	end
+
+	local co = coroutine.create(function()
+		local arg_inside_co = "arg1"
+		helper()
+	end)
+
+	local status, err_msg = coroutine.resume(co)
+	assert_false(status)
+
+	local trace = STP.stacktrace(co)
+	assert_match("arg_inside_co", trace)
+	assert_match("arg_helper", trace)
+end
