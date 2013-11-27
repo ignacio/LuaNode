bbd36c85ca327e2750ab52133d50afec1252a9ee
Ignacio Burgueño <ignaciob@inconcertcc.com>
2013-11-27 15:26:51 -0800

    Don't use --hide in ls
    OS X's default `ls` doesn't have a `--hide` option. Thanks @adelgado

--- test/run.sh
+++ test/run.sh
@@ -4,12 +4,12 @@ set -e
 
 declare -i noProcessed=0
 
-for i in $( ls -1 --hide=disabled simple ); do
+for i in $( ls -1 simple/*.lua ); do
 	echo -e "\033[32mRunning test case: simple."$i"\033[0m"
 	#valgrind --error-exitcode=1 --quiet --gen-suppressions=yes --tool=helgrind luanode run.lua simple.$i
 	#valgrind --error-exitcode=1 --db-attach=yes --quiet --suppressions=suppressions.supp --leak-check=full --show-reachable=yes --tool=memcheck luanode run.lua simple.$i
 	#valgrind --error-exitcode=1 --num-callers=30 --leak-check=full --show-reachable=yes --suppressions=suppressions.supp --gen-suppressions=yes luanode run.lua simple.$i
-	luanode_d run.lua simple.$i
+	luanode_d run.lua $i
 
 	if [ $? -ne 0 ]
 	then
