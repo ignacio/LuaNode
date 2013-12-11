b09a2404ad883656d0c9b64f3d57b37c3a00f220
Ignacio Burgueño <ignaciob@inconcertcc.com>
2013-12-11 10:06:35 -0200

    wip

--- lib/luanode/https.lua
+++ lib/luanode/https.lua
@@ -18,6 +18,9 @@ luanode.https = _M
 
 -- Server Class
 local Server = Class.InheritsFrom(Tls.Server)
+
+--
+-- public
 _M.Server = Server
 
 function Server:__init (options, requestListener)
@@ -32,18 +35,27 @@ function Server:__init (options, requestListener)
 	-- http://wiki.squid-cache.org/SquidFaq/InnerWorkings#What_is_a_half-closed_filedescriptor.3F
 	newServer.httpAllowHalfOpen = false
 
+	newServer:on("clientError", function(self, err, conn)
+		conn:destroy(err)
+	end)
+
 	return newServer
 end
 
-
-local function createConnection (options)
-	return Tls.connect(options)
+--
+-- public
+_M.createServer = function (options, requestListener)
+	return Server(options, requestListener)
 end
 
 
 ---
 -- HTTPS Agents.
 --
+
+local function createConnection (options)
+	return Tls.connect(options)
+end
 
 local Agent = Class.InheritsFrom(Http.Agent)
 --
@@ -73,10 +85,25 @@ _M.request = function (options, cb)
 		error("Protocol: " .. options.protocol .. " not supported.")
 	end
 
-	options.agent = options.agent or globalAgent
-	
-	options.createConnection = createConnection
 	options.defaultPort = options.defaultPort or 443
+	options.createConnection = createConnection
+
+	if options.agent == nil then
+		if  options.ca == nil and
+			options.cert == nil and
+			options.ciphers == nil and
+			options.key == nil and
+			options.passphrase == nil and
+			options.pfx == nil and
+			options.rejectUnauthorized == nil
+		then
+			options.agent = globalAgent
+		else
+			options.agent = Agent(options)
+		end
+
+	end
+
 	return Http.ClientRequest(options, cb)
 end
 
