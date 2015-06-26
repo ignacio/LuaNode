e9aaf26730e4b3e0a84b07312bb2678eaf72902a
Ignacio Burgueño <ignaciob@inconcertcc.com>
2015-06-26 16:26:19 -0300

    matrix

--- ci/travis/install_luarocks.sh
+++ ci/travis/install_luarocks.sh
@@ -10,7 +10,12 @@ pushd "$HOME/build_deps"
 	#   Install a recent luarocks release (in the same place as our Lua install)
 	curl --location http://luarocks.org/releases/luarocks-$LUAROCKS_VER.tar.gz | tar xz
 	pushd luarocks-$LUAROCKS_VER
-		./configure --with-lua="$LUA_DIR" --prefix="$LUA_DIR" --lua-version=$LUA_SHORTV
+		if [ "$COMPILE_LUA" == "yes" ]; then
+			#./configure --with-lua="$LUA_DIR" --prefix="$LUA_DIR" --lua-version=$LUA_SHORTV
+			./configure --with-lua="$LUA_DIR" --prefix="$LUAROCKS_DIR" --lua-version=$LUA_SHORTV
+		else
+			./configure --prefix="$LUAROCKS_DIR" --lua-version=$LUA_SHORTV
+		fi
 		make build && make install
 	popd
 popd
