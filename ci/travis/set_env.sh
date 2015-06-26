0834dfa1b74ad56b3fb92a4243ae51bac02ca530
Ignacio Burgueño <ignaciob@inconcertcc.com>
2015-06-26 17:05:13 -0300

    matrix

--- ci/travis/set_env.sh
+++ ci/travis/set_env.sh
@@ -3,8 +3,8 @@
 if [ "$COMPILE_LUA" == "yes" ]; then
 	# defines LUA_DIR so CMake can find this Lua install
 	export LUA_DIR="$HOME/lua/$LUA_SHORTV"
-	export PATH=${PATH}:${LUA_DIR}/bin
+	export PATH=${LUA_DIR}/bin:${PATH}
 fi
 
 export LUAROCKS_DIR="$HOME/luarocks/$LUA_SHORTV"
-export PATH=${PATH}:${LUAROCKS_DIR}/bin
+export PATH=${LUAROCKS_DIR}/bin:${PATH}
