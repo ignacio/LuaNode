6cf4c371b8d719e8142f85133d6e2ddb4226c764
Ignacio Burgueño <ignaciob@inconcertcc.com>
2015-06-26 17:33:25 -0300

    matrix

--- ci/travis/build.sh
+++ ci/travis/build.sh
@@ -4,7 +4,7 @@ set -eufo pipefail
 
 pushd $TRAVIS_BUILD_DIR/build
 if [ "$COMPILE_LUA" == "yes" ]; then
-	ls -lahr ${LUA_DIR}
+	# be explicit, or else CMake may pick the Lua package from the os)
 	cmake -DBOOST_ROOT=/usr/lib -DLUA_INCLUDE_DIR=${LUA_DIR}/include -DLUA_LIBRARY=${LUA_DIR}/lib/liblua.a ..
 else
 	cmake -DBOOST_ROOT=/usr/lib ..
--- ci/travis/install_luarocks.sh
+++ ci/travis/install_luarocks.sh
@@ -5,11 +5,10 @@ set -efo pipefail
 mkdir -p "$LUAROCKS_DIR"
 
 pushd "$HOME/build_deps"
-	#   Install a recent luarocks release (in the same place as our Lua install)
+	#   Install a recent luarocks release
 	curl --location http://luarocks.org/releases/luarocks-$LUAROCKS_VER.tar.gz | tar xz
 	pushd luarocks-$LUAROCKS_VER
 		if [ "$COMPILE_LUA" == "yes" ]; then
-			#./configure --with-lua="$LUA_DIR" --prefix="$LUA_DIR" --lua-version=$LUA_SHORTV
 			./configure --with-lua="$LUA_DIR" --prefix="$LUAROCKS_DIR" --lua-version=$LUA_SHORTV
 		else
 			./configure --prefix="$LUAROCKS_DIR" --lua-version=$LUA_SHORTV
