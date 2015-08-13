e500675afb79ce241ca5761d4abb7d68c01254c1
Ignacio Burgueño <ignaciob@inconcertcc.com>
2015-08-13 17:11:54 -0300

    Test with Boost 1.59.0

--- ci/appveyor/build.bat
+++ ci/appveyor/build.bat
@@ -24,12 +24,12 @@ cd %build_dir%
 :: Where to put the resulting artifacts
 if not defined ARTIFACTS set ARTIFACTS=%build_dir%\artifacts
 
-if not defined BOOST_VERSION set BOOST_VERSION=1.58.0
+if not defined BOOST_VERSION set BOOST_VERSION=1.59.0
 :: replace dots with underscores
 set BOOST_VER_USC=%BOOST_VERSION:.=_%
 
 cmake --version
-cmake -G "Visual Studio 12 2013" -DBOOST_ROOT="c:\local\boost_%BOOST_VER_USC%" -DBOOST_LIBRARYDIR="c:\local\boost_%BOOST_VER_USC%\lib%arch%-msvc-12.0" -DCMAKE_BUILD_TYPE=Release %_cmake_per_Arch_Args% ..
+cmake -DBOOST_ROOT="c:\local\boost_%BOOST_VER_USC%" -DBOOST_LIBRARYDIR="c:\local\boost_%BOOST_VER_USC%\lib%arch%-msvc-14.0" -DCMAKE_BUILD_TYPE=Release %_cmake_per_Arch_Args% ..
 cmake --build . --config Release
 
 copy %build_dir%\Release\luanode.exe luanode.exe
